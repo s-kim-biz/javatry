755d747e6388d5f80c4caa17f0989cac9ad59918
s.kim <s.kim@bizreach.co.jp>
2020-05-21 09:41:16 +0900

    Step12 test_substring_findFirstChar Done and revise

--- src/test/java/org/docksidestage/javatry/colorbox/Step12StreamStringTest.java
+++ src/test/java/org/docksidestage/javatry/colorbox/Step12StreamStringTest.java
@@ -206,11 +206,11 @@ public class Step12StreamStringTest extends PlainTestCase {
                 .flatMap(colorBox -> colorBox.getSpaceList().stream())
                 .map(boxSpace -> boxSpace.getContent())
                 .filter(content -> content instanceof String && ((String)content).endsWith("front"))
+                .map(content -> (String)content)
                 .findFirst()
                 .orElseGet(() -> {
                     throw new RuntimeException("there is no word that ends with 'front'");
                 })
-                .toString()
                 .indexOf("front");
 
         log(position);
@@ -242,11 +242,11 @@ public class Step12StreamStringTest extends PlainTestCase {
                 .flatMap(colorBox -> colorBox.getSpaceList().stream())
                 .map(boxSpace -> boxSpace.getContent())
                 .filter(content -> content instanceof String && match.containsAtLeastTwoTimes((String)content,"ど"))
+                .map(content -> (String)content)
                 .findFirst()
                 .orElseGet(() -> {
                     throw new RuntimeException("there is no word that contains 'ど' at least two times");
                 })
-                .toString()
                 .lastIndexOf("ど");
 
         log(position);
@@ -260,6 +260,19 @@ public class Step12StreamStringTest extends PlainTestCase {
      * (カラーボックスに入ってる "front" で終わる文字列の最初の一文字は？)
      */
     public void test_substring_findFirstChar() {
+        List<ColorBox> colorBoxList = new YourPrivateRoom().getColorBoxList();
+        char startChar = colorBoxList.stream()
+                .flatMap(colorBox -> colorBox.getSpaceList().stream())
+                .map(boxSpace -> boxSpace.getContent())
+                .filter(content -> content instanceof String && ((String)content).endsWith("front"))
+                .map(content -> (String)content)
+                .findFirst()
+                .orElseGet(() -> {
+                    throw new RuntimeException("there is no word that ends with 'front'");
+                })
+                .charAt(0);
+
+        log(startChar);
     }
 
     /**
